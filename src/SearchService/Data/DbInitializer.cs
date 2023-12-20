721a1457cd9ce9d578d75a4bffc14df337e36aa3
Sid <oxyprogrammer@gmail.com>
2023-12-20 18:34:41 +0530

    Implemented the Search Microservice data seeding by polling the Auction Microservice using http sync call. Eww!!

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -19,19 +20,32 @@ public class DbInitializer
                 .CreateAsync();
 
         var count = await DB.CountAsync<Item>();
-        Console.WriteLine($"Checking Db Initialization, Count: {count}");
-        if (count == 0)
+
+
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine($"Returned from auction service: {items.Count}");
+        if (items.Count > 0)
         {
-            Console.WriteLine("No data - will attempt to seed!");
-
-            var itemsData = await File.ReadAllTextAsync("Data/auctions.json");
-            Console.WriteLine(itemsData);
-
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-
-            var items = JsonSerializer.Deserialize<List<Item>>(itemsData, options);
-
             await DB.SaveAsync(items);
         }
+
+
+        // Console.WriteLine($"Checking Db Initialization, Count: {count}");
+        // if (count == 0)
+        // {
+        //     Console.WriteLine("No data - will attempt to seed!");
+
+        //     var itemsData = await File.ReadAllTextAsync("Data/auctions.json");
+        //     Console.WriteLine(itemsData);
+
+        //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemsData, options);
+
+        //     await DB.SaveAsync(items);
+        // }
     }
 }
