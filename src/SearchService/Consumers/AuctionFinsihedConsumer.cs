9e8c946358ea4d210132d6c9726d1b06431b5719
Sid <oxyprogrammer@gmail.com>
2023-12-22 12:56:02 +0530

    Created new consumer events in search and auction. And added client type in identity.

---
+++ src/SearchService/Consumers/AuctionFinsihedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold && context.Message.Amount.HasValue)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount.Value;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+}
