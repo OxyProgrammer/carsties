d77d6b94c0b2b6f4a03bd6e0521f58f251d49273
Sid <oxyprogrammer@gmail.com>
2023-12-23 15:26:10 +0530

    Completed the home screen with timers over images.

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,8 +1,9 @@
+import Listings from "./Auctions/Listings";
+
 export default function Home() {
-  console.log('Server component');
   return (
     <div>
-      <h3 className='text-3xl font-semibold'>Content goes here.</h3>
+      <Listings/>
     </div>
   );
 }
