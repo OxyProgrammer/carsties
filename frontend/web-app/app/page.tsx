f4147528354b856782933396ba217e2665451cfd
Sid <oxyprogrammer@gmail.com>
2023-12-23 13:47:58 +0530

    Created the frontend web app and started with Navbar.

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  console.log('Server component');
+  return (
+    <div>
+      <h3 className='text-3xl font-semibold'>Content goes here.</h3>
+    </div>
+  );
+}
