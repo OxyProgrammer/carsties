7fd85cedb1b6ae83ba06eef08d4d04d9490b5faa
Sid <oxyprogrammer@gmail.com>
2023-12-24 11:46:21 +0530

    Improved UX for search functionality.

---
+++ frontend/web-app/app/Nav/Search.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { FaSearch } from 'react-icons/fa';
+export default function Search() {
+  const setParams = useParamsStore(state => state.setParams);
+  const setSearchValue = useParamsStore(state => state.setSearchValue);
+  const searchValue = useParamsStore(state => state.searchValue);
+
+  return (
+    <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
+      <input
+        type='text'
+        onKeyDown={(e: any) => {
+          if (e.key === 'Enter') {
+            setParams({ searchTerm: searchValue });
+          }
+        }}
+        className='
+            flex-grow
+            pl-5
+            bg-transparent
+            focus:outline-none
+            border-transparent
+            focus:border-transparent
+            focus:ring-0
+            text-sm
+            text-gray-500
+        '
+        placeholder='Search for cars my make, model or color'
+        value={searchValue}
+        onChange={(e: any) => setSearchValue(e.target.value)}
+      />
+      <button onClick={(e) => setParams({ searchTerm: searchValue })}>
+        <FaSearch
+          size={34}
+          className='bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2'
+        />
+      </button>
+    </div>
+  );
+}
