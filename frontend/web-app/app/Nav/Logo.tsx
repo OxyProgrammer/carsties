daf1a89abe95a734fb2ecc561621a40be3f9347f
Sid <oxyprogrammer@gmail.com>
2023-12-24 17:24:10 +0530

    Implemented Next auth and login-logout functionality.

--- frontend/web-app/app/Nav/Logo.tsx
+++ frontend/web-app/app/Nav/Logo.tsx
@@ -1,13 +1,25 @@
 'use client';
 import { useParamsStore } from '@/hooks/useParamsStore';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 import { AiOutlineCar } from 'react-icons/ai';
 
 export default function Logo() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   const reset = useParamsStore((state) => state.reset);
+  
+  function doReset() {
+    if (pathname !== '/') {
+      router.push('/');
+    }
+    reset()
+  }
+
   return (
     <div
-      onClick={(e) => reset()}
+      onClick={(e) => doReset()}
       className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'
     >
       <AiOutlineCar size={34} />
